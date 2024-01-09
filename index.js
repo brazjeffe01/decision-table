5e5dd1710acca190b046f39d65f7973f93ba8c4f
Jefferson Braz <brazjeffe@gmail.com>
2024-01-09 12:22:14 -0300

    Adicionando mais cenários e mais condições

--- index.js
+++ index.js
@@ -1,26 +1,74 @@
-console.log('FEATURE: Sacar dinheiro!')
+const firstFeature = () => {
 
-const firstScenario = () => {
-    //Código sera refatorado futuramente quando eu finalmente descobrir como receber dados do usuário usando ES6 hahahaha (to triste de vdd)
-    console.log('CONDIÇÕES:')
-    const firstCondition = () => {
-    console.log('Eu estou logado na conta: [VERDADEIRO]')
-    return true
-    }
+    console.log('FEATURE: Sacar dinheiro.')
+
+    const firstScenario = () => {
+        //Código sera refatorado futuramente quando eu finalmente descobrir como receber dados do usuário usando ES6 hahahaha (to triste de vdd)
+    
+        console.log('CONDIÇÕES:')
+        const firstCondition = () => {
+        console.log('Eu estou logado na conta: [VERDADEIRO]')
+        return true
+        }
+            
+        const secondCondition = () => {
+        console.log('Eu tenho dinheiro na conta: [VERDADEIRO]')
+        return true
+        }
+    
+        if (firstCondition() && secondCondition()) {
+            console.log('AÇÕES:')
+            console.log('Eu consigo sacar dinheiro: [VERDADEIRO]')
+        }
+    
+        console.log('--- --- --- ---')
         
-    const secondCondition = () => {
-    console.log('Eu tenho dinheiro na conta: [VERDADEIRO]')
-    return true
-    }
-
-    if (firstCondition() && secondCondition()) {
-        console.log('AÇÕES:')
-        console.log('Eu consigo sacar dinheiro: [VERDADEIRO]')
     }
     
+    const secondScenario = () => {
+        //Código sera refatorado futuramente quando eu finalmente descobrir como receber dados do usuário usando ES6 hahahaha (to triste de vdd)
+        
+        console.log('CONDIÇÕES:')
+        const firstCondition = () => {
+        console.log('Eu estou logado na conta: [VERDADEIRO]')
+        return true
+        }
+            
+        const secondCondition = () => {
+        console.log('Eu tenho dinheiro na conta: [FALSO]')
+        return false
+        }
+
+        if (firstCondition() && secondCondition()) {
+            console.log('AÇÕES:')
+            console.log('Eu consigo sacar dinheiro: [VERDADEIRO]')
+        } else {
+            console.log('AÇÕES:')
+            console.log('Eu não consigo sacar dinheiro: [FALSO]')
+        }
+
+        console.log('--- --- --- ---')
+        
+    }
+
+    firstScenario()
+    secondScenario()
 }
 
-firstScenario()
+const secondFeature = () => {
+
+    console.log('FEATURE: Verificar saldo.')
+
+    const firstScenario = () => {
+
+        console.log('CONDIÇÕES:')
+        const firstCondition = () => {
+
+        }
+    }
+}
+
+firstFeature()
 
 
 //Condição sacarDinheiro
